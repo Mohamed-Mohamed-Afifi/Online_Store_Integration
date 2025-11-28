029d4eb4f3900586750ab18f45371d884069c15a
Mohamed Afify <mohamed.org050@gmail.com>
2025-11-28 11:37:53 +0200

    Removing lombok and mapstuct

--- services/customer/src/main/java/com/ecomm/customer/entity/Customer.java
+++ services/customer/src/main/java/com/ecomm/customer/entity/Customer.java
@@ -2,17 +2,38 @@ package com.ecomm.customer.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
 public class Customer {
     @Id
     private Integer id;
     private String name;
     private String type;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
